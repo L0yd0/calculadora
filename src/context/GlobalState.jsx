9c8df86f3c5864d997c31ee794d576241b73a823
Ricardo Vargas <ricardovargas120717@gmail.com>
2025-11-08 23:41:43 -0600

    act

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react-refresh/only-export-components */
-import React, {createContext, useReducer} from "react";
+import React, {createContext, useReducer, useEffect} from "react";
 import AppReducer from "./AppReducer";
 //Initial state
 const initialState = {
     movimientos:[
         //esto es lo que supuestamente debe de mostrar en movimientos
         //{ id: 1, descripcion: 'Flower', importe: -20 },
-    ]
+    ],
+    loading: true, 
+    error: null 
 }
+
+//url de la api 
+const API_URL = 'https://calculadora-back-d9aj.onrender.com/api/movimientos'
 
 //context creation
 export const GlobalContext = createContext(initialState)
@@ -16,21 +21,49 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({ children }) => {
     const [state,dispatch] = useReducer(AppReducer, initialState)
 
+    //cargamos los datos al iniciar la app
+     useEffect(()=>{
+        getMovimientos()
+    }, [])
     //acciones
-    function deleteMovimiento(id){
-        dispatch({
+    async function getMovimientos(){
+        try {
+            const response = await fetch(`${API_URL}`) 
+            const data = await response.json()
+
+            dispatch({
+                type: 'GET_MOVIMIENTOS',
+                payload: data
+            })
+        } catch (error) {
+            dispatch({ type: 'MOVIMIENTO_ERROR', payload: error.message });
+        }
+    }
+
+    async function deleteMovimiento(id){
+        try {
+            dispatch({
             type: 'DELETE_MOVIMIENTO',
             payload: id
         })
+        } catch (error) {
+            dispatch({ type: 'MOVIMIENTO_ERROR', payload: error.message });
+        }
     }
-    function addMovimiento(movimiento){
-        dispatch({
+     async function addMovimiento(movimiento){
+        try {
+            dispatch({
             type: 'ADD_MOVIMIENTO',
             payload:movimiento
         })
+        } catch (error) {
+            dispatch({ type: 'MOVIMIENTO_ERROR', payload: error.message });
+        }
     }
     return (<GlobalContext.Provider value={{
         movimientos: state.movimientos, 
+        loading: state.loading,
+        error: state.error,
         deleteMovimiento,
         addMovimiento
     }}>
