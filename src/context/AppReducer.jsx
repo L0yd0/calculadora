de1c9e060bae325bf2a673bc1d76af152589dcc4
Ricardo Vargas <ricardovargas120717@gmail.com>
2025-11-08 23:57:27 -0600

    Refactor AppReducer to handle loading state correctly

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,17 +1,32 @@
 const AppReducer = (state, action) => {
-    switch(action.type){
+    switch (action.type) {
+        case 'GET_MOVIMIENTOS':
+            return {
+                ...state,
+                loading: false,
+                movimientos: action.payload
+            }
         case 'DELETE_MOVIMIENTO':
             return {
                 ...state,
-                movimientos: state.movimientos.filter(movimiento => movimiento.id !== action.payload)
+                loading: false,
+                movimientos: state.movimientos.filter(movimiento => movimiento._id !== action.payload)
             }
         case 'ADD_MOVIMIENTO':
             return {
                 ...state,
+                loading: false,
                 movimientos: [action.payload, ...state.movimientos]
+            }
+        case 'MOVIMIENTO_ERROR':
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
             }
         default:
             return state
     }
 }
+
 export default AppReducer
